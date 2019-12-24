703417a9a6cf35f1f9ccb8ea6913930bc7033ba9
Yotam Medini <yotam.medini@n2ws.com>
2019-12-24 11:46:32 +0200

    sqrmagic in development

---
+++ sqrmagic.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <string>
+#include <cstdlib>
+using namespace std;
+typedef vector<int> vi_t;
+
+int main(int argc, char **argv) {
+  int rc = 0;
+  int n = stoi(argv[1]);
+  return rc;
+}
